0adf2a8a77f1ed224fa973f3010438aeec645dc0
Syed Muhammad Hussain <97514703+hussain1406@users.noreply.github.com>
2023-10-28 15:32:07 +0500

    fixed dark mode issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Alert from "./components/Alert";
 import { Switch, Route } from "react-router-dom";
 import { HashRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
 function App() {
-  // const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
@@ -19,34 +19,33 @@ function App() {
       setAlert(null);
     }, 1500);
   };
-  // const toggleMode = () => {
-  //   if (mode === "light") {
-  //     setMode("dark");
-  //     document.body.style.backgroundColor = "#373737";
-  //     showAlert("Dark Mode has been Enabled Successfully", "success");
-  //     return;
-  //   }
-  //   setMode("light");
-  //   document.body.style.backgroundColor = "white";
-  //   showAlert("Dark Mode has been Disabled Successfully", "success");
-  // };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#373737";
+      showAlert("Dark Mode has been Enabled Successfully", "success");
+      return;
+    }
+    setMode("light");
+    document.body.style.backgroundColor = "white";
+    showAlert("Dark Mode has been Disabled Successfully", "success");
+  };
   return (
     <Router>
+      <Navbar title="TextUtils" mode={mode} />
       {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} /> */}
-      <Navbar title="TextUtils" />
       <div className="container my-3">
         <Alert alert={alert} />
         <Switch>
           <Route exact path="/">
             <TextForm
               heading="Enter the text to be analyzed:"
-              // mode={mode}
+              mode={mode}
               showAlert={showAlert}
             />
           </Route>
           <Route exact path="/about">
-            <About />
-            {/* <About mode={mode} /> */}
+            <About mode={mode} />
           </Route>
         </Switch>
       </div>
